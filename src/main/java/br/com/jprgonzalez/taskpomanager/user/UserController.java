c9b78498a350f621394c3698a135fb8def26d406
Juan Gonzalez <juan.gonzalez@edu.unifil.br>
2025-08-16 01:51:38 -0300

    feat: add UserController with create endpoint

---
+++ src/main/java/br/com/jprgonzalez/taskpomanager/user/UserController.java
@@ -0,0 +1,30 @@
+package br.com.jprgonzalez.taskpomanager.user;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jprgonzalez.taskpomanager.user.dto.UserCreationRequestDTO;
+import br.com.jprgonzalez.taskpomanager.user.dto.UserResponseDTO;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserCreationRequestDTO userRequestDTO) {
+        UserResponseDTO createdUser = userService.createUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+}
