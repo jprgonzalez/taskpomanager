a29863362a738d98ab69de3d36de6bddb4d42ce3
Juan Gonzalez <juan.gonzalez@edu.unifil.br>
2025-08-16 01:35:16 -0300

    initial commit

---
+++ src/test/java/br/com/jprgonzalez/taskpomanager/TaskpomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jprgonzalez.taskpomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskpomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
