b68bdf689bd4b841ce6311598313a9a387dcfb42
Juan Gonzalez <juan.gonzalez@edu.unifil.br>
2025-08-17 00:34:17 -0300

    feat: add get user method to UserService

--- src/main/java/br/com/jprgonzalez/taskpomanager/user/UserService.java
+++ src/main/java/br/com/jprgonzalez/taskpomanager/user/UserService.java
@@ -1,9 +1,13 @@
 package br.com.jprgonzalez.taskpomanager.user;
 
+import java.util.Objects;
+import java.util.stream.Stream;
+
 import org.springframework.stereotype.Service;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.jprgonzalez.taskpomanager.exceptions.custom.UserAlreadyExistsException;
+import br.com.jprgonzalez.taskpomanager.exceptions.custom.UserNotExistsException;
 import br.com.jprgonzalez.taskpomanager.user.dto.UserCreationRequestDTO;
 import br.com.jprgonzalez.taskpomanager.user.dto.UserResponseDTO;
 
@@ -52,6 +56,44 @@ public class UserService {
         );
     }
 
+    public UserResponseDTO getUser(Long id, String username, String email) {
+        long nonNullCount = Stream.of(id, username, email).filter(Objects::nonNull).count();
+        if (nonNullCount == 0) {
+            throw new UserNotExistsException("At least one parameter (id, username, or email) must be provided.");
+        }else if (nonNullCount > 1) {
+            throw new UserNotExistsException("Only one parameter (id, username, or email) should be provided.");
+        }
 
+        UserModel foundUser;
+
+        if (id != null) {
+            foundUser = userRepository.findById(id)
+                    .orElseThrow(() -> new UserNotExistsException("User not found by id: " + id));
+        } else if (username != null) {
+            foundUser = userRepository.findByUsername(username)
+                    .stream()
+                    .findFirst()
+                    .orElseThrow(() -> new UserNotExistsException("User not found by username: " + username));
+        } else {
+            foundUser = userRepository.findByEmail(email)
+                    .stream()
+                    .findFirst()
+                    .orElseThrow(() -> new UserNotExistsException("User not found by email: " + email));
+        }
+
+        return new UserResponseDTO(
+            foundUser.getId(),
+            foundUser.getName(),
+            foundUser.getUsername(),
+            foundUser.getEmail(),
+            foundUser.getRole(),
+            foundUser.getTeamId(),
+            foundUser.getActive(),
+            foundUser.getCreatedAt(),
+            foundUser.getCreatedByUserId(),
+            foundUser.getLastModifiedAt(),
+            foundUser.getLastModifiedByUserId()
+        );
+    }
 
 }
