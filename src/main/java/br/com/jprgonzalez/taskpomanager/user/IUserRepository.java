4e3053ce30d46e8d4957d575c86a327a0d04476b
Juan Gonzalez <juan.gonzalez@edu.unifil.br>
2025-08-17 00:31:10 -0300

    feat: add find by id to user model

--- src/main/java/br/com/jprgonzalez/taskpomanager/user/IUserRepository.java
+++ src/main/java/br/com/jprgonzalez/taskpomanager/user/IUserRepository.java
@@ -2,9 +2,11 @@ package br.com.jprgonzalez.taskpomanager.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface IUserRepository extends JpaRepository<UserModel, Long> {
     List<UserModel> findByUsername(String username);
     List<UserModel> findByEmail(String email);
+    Optional<UserModel> findById(Long id);
 }
